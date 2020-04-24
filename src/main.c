eed03c6652927e8aeb913b9b9bf2238b039c2059
Eric Macià <ericmaciared@gmail.com>
2020-04-24 19:57:03 +0200

    First Push Added: main.c rfid.c/rfid.h Modified: - Removed: -

---
+++ src/main.c
@@ -0,0 +1,31 @@
+/*
+ * File:   main.c
+ * Author: Eric Macià (eric.macia)
+ *         Guillermo Sabaté (guillermo.sabate)
+ *
+ * Created on 24/04/2020
+ */
+
+//includes
+#include "RFID.h"
+
+
+// Configuration Bits
+
+
+//Main Config
+void initCPU(){
+
+}
+
+
+
+
+int main(void){
+    //inits
+
+    while(1){
+        //motors
+
+    }
+}
